fbb3d610f337cd5bc84046d69b4a3ef4ae08d5a4
Tyler Anderson <unlimitedbacon@gmail.com>
2014-07-13 20:13:35 -0700

    Send some commands to client

--- spore.py
+++ spore.py
@@ -1,6 +1,10 @@
+#!/usr/bin/python
+#
 # Spore
 #
 # Matchmaking and relay server for Fungus.
+
+from random import randint
 
 from twisted.internet import protocol, reactor
 from twisted.protocols.basic import LineReceiver
@@ -15,7 +19,9 @@ class FungusProtocol(LineReceiver):
 		# If data is received before the connection is made (why would that ever happen?)
 		# then this ID will be invalid
 		self.name = None						# Username
+		self.req_players = 2						# Requested number of players for game
 		self.state = "UNCONNECTED"					# Possible states: UNCONNECTED, LOGIN, WAITING, GAME
+		self.login_request = None					# Current piece of information being requested for login
 		self.game = None						# Game this player is playing
 	
 	def transmit(self, message):
@@ -35,6 +41,7 @@ class FungusProtocol(LineReceiver):
 		# Log incoming IP address
 		self.ip = self.transport.getPeer().host
 		self.port = self.transport.getPeer().port
+		print( ':: Connection from %s:%i' % (self.ip,self.port) )
 
 		# Add to list of connections
 		self.factory.numConnections += 1				# Count the number of open connections
@@ -45,7 +52,8 @@ class FungusProtocol(LineReceiver):
 		self.factory.connections[ self.num ] = self			# Add this connection to the list
 		
 		# Change state
-		self.state = "LOGIN"						# Begin waiting for username
+		self.state = 'LOGIN'						# Start login process
+		self.login_request = 'USERNAME'					# Begin waiting for username
 
 		# Send welcome message
 		self.transmit( '\r\nWelcome to the fungal server' )
@@ -53,36 +61,55 @@ class FungusProtocol(LineReceiver):
 		self.transmit( 'There are currently %i connections' % (self.factory.numConnections) )
 		self.transmit( 'This is connection number %i' % (self.num) )
 		self.transmit( 'Address: %s Port: %i' % (self.ip, self.port) )
-		message = 'Username: '
-		self.transport.write( message.encode("utf-8") )
+		self.transmit( 'USERNAME:' )					# Text in CAPS is client commands
 	
 	def connectionLost(self, reason):
+		print( ':: Disconnect %s at %s:%i' % (self.name,self.ip,self.port) )
 		self.factory.numConnections -= 1				# Remove connection from count
 		del self.factory.connections[ self.num ]			# Remove self from list
 
-		# Inform all other players of disconnect
-		self.txOtherPlayers( 'Player %i disconnected.' % (self.game.index(self)) )
-
-		# Remove self from game
-		self.game.remove(self)
-		self.factory.checkEndgame( self.game )
+		if self.game:
+			# Inform all other players of disconnect
+			self.txOtherPlayers( 'Player %i disconnected.' % (self.game.index(self)) )
+			# Remove self from game
+			self.game.remove(self)
+			self.factory.checkEndgame( self.game )
 
 	#def dataReceived(self, data):
 	def lineReceived(self, data):
-		data = data.decode("utf-8")					# Convert bytestring back to normal (unicode) string
+		data = data.decode('utf-8')					# Convert bytestring back to normal (unicode) string
 
-		if self.state == "LOGIN":
+		if self.state == 'LOGIN':
 			self.login(data)
-		elif self.state == "GAME":
+		elif self.state == 'GAME':
 			self.relay(data)
 	
 	def login(self, data):
-		self.name = data
-		self.state = "WAITING"
-		self.transmit( 'Access granted, %s.' % (self.name) )
-		self.factory.newGame.append( self )
-		if len(self.factory.newGame) >= maxPlayers:
-			self.factory.startGame()
+		if self.login_request == 'USERNAME':
+			self.name = data					# Set username
+			self.login_request = 'NUM_PLAYERS'			# Begin waiting for requested number of players
+			self.transmit( 'NUM_PLAYERS:' )
+		elif self.login_request == 'NUM_PLAYERS':
+			try:
+				self.req_players = int(data[0])			# Set number of players in game
+			except ValueError:					# Make sure client is sending integers
+				self.transmit( 'Error: That was not a number. Try again.' )
+				return
+			# Make sure value is within range
+			#if self.req_players < 2 or self.req_players > 4:
+			#	self.transmit( 'Error: Must choose either 2, 3, or 4 players.' )
+			#	return
+			if self.req_players != 2:
+				self.transmit( 'ERROR: Only supporting 2 player games right now' )
+				return
+			# End login process and start game
+			self.login_request = None
+			self.state = 'WAITING'
+			self.transmit( 'Access granted, %s.' % (self.name) )
+			print( ':: Login from %s for %i player game' % (self.name,self.req_players) )
+			self.factory.newGame.append( self )			# Add player to staging game
+			if len(self.factory.newGame) >= maxPlayers:		# Start game if it has enough players
+				self.factory.startGame()
 
 	def relay(self, data):
 		# Interpret commands
@@ -106,16 +133,23 @@ class FungusFactory(protocol.Factory):
 		game = self.newGame
 		self.games.append(game)						# Move game to in progress
 		self.newGame = []						# Reset staging game
+		start_player = randint(0,maxPlayers)				# Choose random starting player
+		print( ':: Game #%i starting between ' % (self.games.index(game)) , end="" )
 		for player in game:
+			print(player.name, end=", ")
 			player.game = game
 			player.state = "GAME"
 			player.transmit( 'Enough players have arrived. Game started' )
 			player.transmit( 'Your are player number %i' % (game.index(player)) )
+			player.transmit( 'YOUR_NUM: %i' % (game.index(player)) )
+			player.transmit( 'START: %i' % (start_player) )
+		print( 'starting with player %i' % (start_player) )
 	
 	def checkEndgame(self, game):
 		# If all but one player has disconnected
 		# ( do not make this <=1 or it will cause a loop )
 		if len(game) == 1:
+			print( ':: Game #%i ended' % (self.games.index(game)) )
 			# Disconnect all remaining players
 			for player in game:
 				player.transmit( 'Game Over' )
